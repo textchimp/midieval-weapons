feb54a6c7925a7d9078c085c32e7314d538b5686
Luke Hammer <textchimp@gmail.com>
2018-07-28 22:00:48 +1000

    working tuplets calcuation (i think), separated into own functions, first 3 measure in new notation scored

--- sonicpi/volans-shiva.pi.rb
+++ sonicpi/volans-shiva.pi.rb
@@ -1,6 +1,15 @@
 use_midi_defaults port: 'linuxsampler_in_0'
-set_sched_ahead_time! 1
+set_sched_ahead_time! 0.1
+
+require_relative '/Users/textchimp/.rvm/gems/ruby-2.4.1/gems/colorize-0.8.1/lib/colorize'
+
+clc "START______________________"
+
 use_bpm 45
+
+def pretty(o)
+  JSON.pretty_generate(o)
+end
 
 # durations lookup
 @dur = Hash.new(0.25)
@@ -8,12 +17,8 @@ use_bpm 45
 @dur['s'] = 0.0625
 @dur['e'] = 0.125
 @dur['q'] = 0.25
-@dur['q/5'] = @dur['q']/5.0
 @dur['h'] = 0.5
-@dur['h/5'] = @dur['h']/5.0
 @dur['w'] = 1
-@dur['w2/5'] = 2*@dur['q']/5.0
-@dur['w1/5'] = @dur['q']/5.0
 
 # velocity lookup
 @vel = Hash.new(90)
@@ -37,69 +42,119 @@ def mplay(args  = {})  # **args
   midi args[:note], **args
 end
 
-# hash of notes, indexed by measure number
-@score = {}
+def check_set_measure(token)
+  return false unless token =~ /(\d+)\:/
+  $1 #return measure num
+end
 
-def check_set_score_hash_measure(score_hash, chord)
-  return false unless chord =~ /(\d+)\:/
-  measure = $1
-  score_hash[measure] = []
-  measure
+def get_duration( str )
+
+  mult = 1.05 #1.1
+
+  f = str.to_f
+  return f if f > 0
+
+  # cl "------ GOT legato, #{@dur[str]} -> #{@dur[str] * mult}"
+  if str.end_with?('_')
+    dur = @dur[ str.chomp('_') ]
+    cl "RET #{[ (dur * mult), dur ]}".blue
+    return [ (dur * mult), dur ]  # return unique duration and sleep time as array
+  end
+
+  @dur[str]  # return single duration value
+end
+
+def generate_tuplet_timings(tuplets, beats, noteval, num_tuplets)
+  beats = beats.to_i
+  noteval = @dur[ noteval ]   # beats/noteval is the time signature
+  num_tuplets = num_tuplets.to_f
+  # cl "FOUND END #{beats} / #{noteval} : #{num_tuplets}"
+  # cl "Tuplets INPUT: #{tuplets.inspect}".blue
+  # # cl "note: '#{note.inspect}', dur: '#{dur.inspect}', vel: '#{vel.inspect}', beats: '#{beats.inspect}'"
+  # beat_factor =  0.85
+  # # goal is 0.1  .... 0.8 ??
+
+  # pure trial and error, no idea *why* this is true, just seems to give
+  # correct value when multipled by note duration
+  # (i.e. mult = 0.8 for 2/4 time, with tuplets of 5)
+  mult = (beats / num_tuplets) / (beats * noteval)
+
+  tuplets.map do |notes|
+    notes.map do |n, d, v|
+      dur = d.is_a?(Array) ? d.map{ |e| e * mult } : d * mult
+      [n, dur, v]
+    end
+  end
+end
+
+def check_for_tuplets(token, tuplets)
+  case token
+  when '['
+    # START of tuplet group; [] indicates tuplets tracking array should be cleared
+    []
+  when /\](\d+)([a-z]+)(\d+)/
+    # end of tuplets group, return array of notes with adjusted durations
+    generate_tuplet_timings(tuplets, $1, $2, $3)
+  else
+    nil  # not found
+  end
 end
 
 def score_parse(s, key=nil)
   # process each chord-or-note
+  measure = -1
+  tuplets_from = nil
+  hand  = :r
 
-  @score[key] = {}   # score hash for the given key
-  measure = 0
-  timing_group = nil
-
-  s.inject([]) do |acc,chord|
+  s.inject( {l: [], r: []} ) do |score_acc, token|
+    # cl "start: ", score_acc.inspect
 
     # check for N: measure numbers
-    if m = check_set_score_hash_measure(@score[key], chord)
-      measure = m
-      next acc
+    if check_set_measure(token)
+      measure += 1
+      hand = :r
+      # cl "RIGHT, #{ measure }"
+      score_acc[:r][measure] = []
+      next score_acc
     end
 
-    # check for '[ / ]N' timing change brackets
-    case chord
-    when '['
-      timing_group = []
-      next acc
-    when /\](\d+)/
-      beats = ($1).to_f
-      cl "FOUND END #{beats}"
-      cl timing_group.inspect
-      notes = timing_group.map do |notes|
-        notes.map do |note, dur, vel|
-          cl "note: '#{note.inspect}', dur: '#{dur.inspect}', vel: '#{vel.inspect}', beats: '#{beats.inspect}'"
-          cl "OUT", [note, dur/beats, vel].inspect
-          [note, dur/beats, vel]
-        end
+    # switch from right to left hand (i.e. treble to bass clef) of same measure
+    if token == ':'
+      hand = :l
+      # cl "LEFT, #{ measure }"
+      score_acc[:l][measure] = []
+      next score_acc
+    end
+
+    # check for [ a b c ]1q5 tuplet notation (TODO: neater return signalling from this fn)
+    if t = check_for_tuplets(token, tuplets_from)
+      if t.any? # array of notes returned indicates tuplet group finished; append to score
+        score_acc[hand][measure] += t
+        tuplets_from = nil  # reset to 'not in tuplet group' state
+      else
+        tuplets_from = [] # empty array indicated new tuplet group, so reset tracking array
       end
-      cl "notes:", notes.inspect
-      timing_group = false
-      acc += notes
-      cl "t-acc", acc.inspect
-      next acc
+
+      # cl "t = #{ t.inspect }".light_green
+      next score_acc  # skip further process for this token
     end
+    # cl "tuplets_from #{tuplets_from.inspect}"
 
-    notes, dur, vel = chord.split('-')
-    dur = dur.to_f > 0 ? dur.to_f : @dur[ dur ] # use literal number, otherwise lookup symbol
-    vel = vel.to_f > 0 ? vel.to_f : @vel[ vel ]
+    notes, dur, vel = token.split('-')
+    dur = get_duration(dur)
+    # cl "DURATION GOT: #{ dur.inspect }, #{ dur.class }".yellow
+    vel = vel.to_f > 0 ? vel.to_f : @vel[ vel ] # use literal value if given, else lookup
 
-    if timing_group
-      timing_group << split_notes(notes, dur, vel)   # TODO: do we actually want += and not <<  ???????
-      acc
+    if tuplets_from
+      # cl "SPLIT: ", split_notes(notes, dur, vel).inspect; # cl "TUP PUSH", tuplets_from.inspect
+      tuplets_from << split_notes(notes, dur, vel)
     else
-      n = split_notes(notes, dur, vel)
-      @score[key][measure] << n
-      acc << n
-      cl "acc", acc.inspect
-      acc
+      # cl "err: hand #{hand} measure #{measure}"
+      # cl "score", score_acc.inspect
+      score_acc[hand][measure] << split_notes(notes, dur, vel)  # standard append of notes
     end
 
+    score_acc
   end # inject
 end
 
@@ -117,16 +172,42 @@ end
 # play a single chord or note from the score array
 def play_score_note(notes, opts = {})
   opts[:vscale] ||= 1.0
+  # cl "NOTES: #{notes.inspect}".red
+  zzz = nil
+  z_candidate = nil
   notes.each do |n|
-    mplay note:n[0], sustain:n[1], velocity:n[2] * opts[:vscale], **opts
+    if n[1].is_a? Array
+      # note with legato value, i.e. different sustain & sleep values
+      # TODO: if chord, last note's legato value will be used? no legato in chords?
+      dur, zzz = n[1]
+      # cl "n1: #{n[1].inspect}, dur: #{dur}, zzz: #{zzz}".green
+    else
+      dur = n[1]
+      z_candidate = n[1]
+    end
+    cl "MPLAY(): note:#{n[0]}, sustain:#{n[1]}, velocity:#{n[2]}"
+    mplay note:n[0], sustain:dur, velocity:n[2] * opts[:vscale], **opts
   end
-  sleep notes[0][1]  # just use the first note's sleep value (TODO: better)
+
+  # TODO: discriminate between
+  # a: how long note should sound  (sustain, above)
+  # b: when next note should start (sleep, below)
+  cl "DUR: #{notes[0][1]}, SLEEP: #{notes[0][1]}"
+  # sleep for either specified legato sleep time, or last note in chord's sleep time
+  sleep zzz || z_candidate
 end
 
-# play a single measure by number
-def play_measure(key, m, opts = {})
-  measure = @score[key][m.to_s]
-  measure.each { |mes| play_score_note(mes, **opts) }
+# play a single measure by number, both hands
+def play_score_measure(score, measure_num, opts = {})
+
+  stop unless score[:l][measure_num] && score[:r][measure_num]
+
+  in_thread do
+    score[:l][measure_num].each { |m| play_score_note(m, **opts) }
+  end
+  # in_thread do    # NO! only one thread, otherwise 0 time elapses!
+    score[:r][measure_num].each { |m| play_score_note(m, **opts) }
+  # end
 end
 
 # L1: [ e4-e_ f4-e f4-e_ b4-e e5-s r-s ]5
@@ -134,75 +215,61 @@ end
 # R1: [ e4-e_ f4-e f4-e_ b4-e e5-s r-s ]5
 # [ b3-e  b3-e b3-e_ d4-e f4-s r-s ]5 ),
 
-score_left =
+# opting for interleaved arrays for L-R pianos because it's a relatively uncommon case
+# 1: [ e4-e_ f4-e f4-e_ b4-e e5-s r-s ]2q5
+# : [ b3-e  b3-e b3-e_ d4-e f4-s r-s ]2q5
+
+score =
 %w(
-  [ e4-e_ f4-e f4-e_ b4-e e5-s r-s ]5
-  [ b3-e  b3-e b3-e_ d4-e f4-s r-s ]5
-
-
-2: a4,e5-w-f          a4,e5-s-a r-s
-   a3,d4-w-f          a3,d4-s-a r-s
-
+1: [ e4-e_ f4-e f4-e_ b4-e e5-s r-s ]2q5
+ : [ b3-e  b3-e b3-e_ d4-e f4-s r-s ]2q5
+),
+%w(
+1: [ d4-e  d4-e d4-e_ f4-e b4-s r-s ]2q5
+ : [ a3-e  a3-e a3-e_ c4-e e4-s r-s ]2q5
+),
+%w(
+2: a4,e5-w-f          a4,e5-s-a r-s r-e
+ : a3,d4-w-f          a3,d4-s-a r-s r-e
+),
+%w(
+2: d5-w-p             r-q
+ : r-w                r-q
+),
+%w(
 3: b5,cs6-w-p         r-q
-   r-w                r-q  (r-wq??)
-
-4: a4,e5-w-f          a4,e5-s r-s
-   a3,d4-w            a3,d4-s r-s
+ : r-w                r-q
+),
+%w(
+3: a4,e5-w-f          a4,e5-s-a r-s r-e
+ : a3,d4-w-p          a3,d4-s-f r-s r-e
 )
 
-# 0:  [ e4,f4-e f4-e f4-e b4-e e5-s r-s ]5
-score_left = %w(
-1:  b3,e4-w2/5 b3,f4-w2/5 b3,f4-w2/5 d4,b4-w2/5 f4,e5-w1/5 r-w1/5
-2:  a3,d4,a4,e5-w-f  a3,d4,a4,e5-s-mf  r-s r-e
-3:  b5,cs6-w-p  r-q
-4:  a3,d4,a4,e5-w-f  a3,d4,a4,e5-s-mf  r-s r-e
-5:  a4,gs5,e6,b6-w-p  r-q
-6:  b3,f4-h/5 b3,e4-q/5 r-q/5  b3,b4-h/5 d4,b4-q/5 r-q/5 f4,e5-q/5 r-q/5  d4,e5-h/5 a4,b5-q/5 r-q/5 b3,e4-h/5 b3,f4-q/5  r-q/5 b3,f4-h/5
-7:  a3,d4,a4,e5-w    a3,d4,a4,e5-s-f  r-s r-e
-8:  e6-w-p                 a4,e5-s-f  r-s r-e
-9:  a3,d4-w-p              a4,e5-s-mf r-s r-e
-10: a3,d4-w-p        a3,d4,a4,e5-s-f  r-s r-e
-11: a3,d4,a4,b5-w-p  a3,d4,a4,e5-s-mf r-s r-e
-12: r-w r-q
-)
 
-# 0:  r-w r-w
-score_right = %w[
-1:  a3,d4-w2/5 a3,d4-w2/5 a3,d4-w2/5 c4,f4-w2/5 e4,b4-w1/5 r-w1/5
-2:  d5-w-p r-q
-3:  a3,d4,a4,e5-w-f   a3,d4,a4,e5-s    r-s r-e
-4:  a3,d4-h/5-p b2,d3,c5,e5-h/5-mf  a3,d4-h/5 b2,d3,c5,e5-h/5-mf  a3,d4-h/5 b2,d3,c5,e5-h/5-mf  a3,d4-h/5 b2,d3,c5,e5-h/5-mf  a3,d4-h/5 b2,d3,c5,e5-h/5-mf  r-q
-5:  a3,d4,a4,e5-w-f   a3,d4,a4,e5-s    r-s r-e
-6:  a3,d4-h/5 a3,d4-q/5 r-q/5  a3,d4-h/5 c4,f4-q/5 r-q/5 e4,b4-q/5 r-q/5  cs4,a4-h/5 d4,e5-q/5 r-q/5 a3,d4-h/5 a3,d4-q/5 r-q/5 a3,d4-q/5
-7:  b5,cs6-w-p    r-q
-8:  a3,d4,a4,e5-w-f   a3,d4-s          r-s r-e
-9:        a4,e5-w-p   a3,d4-s-mf       r-s r-e
-10:       a4,e5-w-p   a3,d4,a4,e5-s-mf r-s r-e
-11: gs5,e6-w-p    r-q
-12: a3,d4,a4,e5-w-f  a3,d4,a4,e5-s-mf  r-s r-e
-]
+piano1 = []
+piano2 = []
+score.each_with_index do |el, i|
+  # separate interleaved (nested, alternating) arrays for two pianos into two separate arrays
+  i.even? ? piano1 += el : piano2 += el
+end
 
-# process score arrays for both pianos
-score_left  = score_parse(score_left,  :l)  # add .ring here to loop
-score_right = score_parse(score_right, :r)
+# inject version
+# piano1, piano2 = score.each_with_index.inject([ [], [] ]) do |acc, (el, index)|
+#   # separate interleaved (nested, alternating) arrays for two pianos into two separate arrays
+#   index.even? ? acc[0] += el : acc[1] += el
+#   acc
+# end
 
-# left piano
+@piano1 = score_parse(piano1)
+@piano2 = score_parse(piano2)
+
+# require 'pp'; cl "S", score.pretty_inspect
+# cl 'j', JSON.pretty_generate(new_score[:l])
+# raise 'done'
+
 live_loop :shiva_l do
-  play_score_note score_left.tick(:l),  c:3, pan:127 #, vscale:0.5
+  play_score_measure @piano1, tick(:l), c:3, pan:127
 end
-
-# right piano
 live_loop :shiva_r do
-  play_score_note score_right.tick(:r), c:3, pan:1
+  play_score_measure @piano2, tick(:r), c:3, pan:1
 end
-
-# to play one measure:
-#
-# m = 2
-# live_loop :gol do
-#   play_measure :l, m, c:3
-# end
-#
-# live_loop :gor do
-#   play_measure :r, m, c:3
-# end
