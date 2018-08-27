405bd1a2741b49918b4076e14b9228efc927d6d8
Luke Hammer <textchimp@gmail.com>
2018-08-27 22:55:50 +1000

    ignore zeroes for touchpad; specify on command line that some values are only sent when a button is held

--- node/daydream-to-midi/main.js
+++ node/daydream-to-midi/main.js
@@ -1,3 +1,8 @@
+// Daydream to MIDI bridge
+
+// Usage:
+// node main.js -d -c 1 -p 'IAC Driver Bus 1'  xOri yOri isAppDown:zOri xTouch yTouch
+
 const easymidi = require('easymidi'),
   daydream     = require('./daydream-node')(),  // requires 'noble'
   argv         = require('minimist')(process.argv.slice(2)),
@@ -18,18 +23,20 @@ const ccMap = {
   },
   zOri: {
     cc: 3,
-    min: -1,
-    max: 1
+    min: -2,
+    max: 2
   },
   xTouch: {
     cc: 4,
     min: 0,
-    max: 1
+    max: 1,
+    ignoreZero: true
   },
   yTouch: {
     cc: 5,
     min: 0,
-    max: 1
+    max: 1,
+    ignoreZero: true
   }
 };
 
@@ -37,12 +44,21 @@ const midiPort =  argv.p || 'LinuxSampler_in_0';
 const channel  = (argv.c || 1) -1;
 
 const readings = argv._.length ?  argv._ : ['xOri'];
-readings.forEach( r => {
+readings.forEach( (r,i) => {
+  if(r.includes(':')){
+    const [cond, key] = r.split(':');
+    console.log(cond, key);
+    ccMap[key].cond = cond;
+    readings[i] = key;
+    r = key;
+  }
   if( !Object.keys(ccMap).includes(r) ){
     console.log(`ERROR: bad reading name '${r}'`);
     process.exit(1);
   }
 });
+
+console.log(ccMap);
 
 console.log('Sending values for: ', readings.join(', '));
 
@@ -51,9 +67,7 @@ console.log(`Channel: ${channel+1}`);
 
 const output = new easymidi.Output(midiPort);
 
-const prevData = {
-  xOri: null
-};
+const prevData = {};
 
 const map_range = (v, a, b, y, z, clamp=true) => {
   if( clamp ){
@@ -69,25 +83,34 @@ console.log('Please press HOME button on controller to connect...');
 
 
 daydream.onStateChange( data => {
-  // output whole data object using JSON.stringify and readline library
-  // (i.e. overwrite output instead of appending)
   let lineJump = 0;
   if( argv.D ){
     // full debug output
     lineJump = 20;
     readline.clearScreenDown(process.stdout);
+    // output whole data object using JSON.stringify and readline library
+    // (i.e. overwrite output instead of appending)
     process.stdout.write(JSON.stringify(data, null, 2));
   }
+
   // send each readings
   readings.forEach( key => {
     const params = ccMap[key];
+
     const value = Math.ceil( map_range(data[key], params.min, params.max, 1, 127) );
-    if( value !== prevData[key] ){
-      output.send('cc', { controller: params.cc, value, channel });
-      prevData[key] = value;
+      if( value !== prevData[key] ){
+        if( !params.ignoreZero || data[key] > 0 ){
+          // only send values which have changed (also make sure that touchpad values which reset to zero are checked)
+          if( !params.cond || data[params.cond] ){
+            // if a button condition is set from command line (ie 'isClickDown:yOri') make sure its true
+            output.send('cc', { controller: params.cc, value, channel });
+            prevData[key] = value;
+            if(argv.d) process.stdout.write(`\n${key}: ${data[key]}, mapped: ${value}             `);
+            lineJump++;
+        }
+      }
     }
-    if(argv.d) process.stdout.write(`\n${key}: ${data[key]}, mapped: ${value}             `);
-    lineJump++;
+
   });
 
   if( argv.d ){
